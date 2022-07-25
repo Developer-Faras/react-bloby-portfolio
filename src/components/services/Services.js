8c9d9137fd0d2eb4ca0c29c27f0de9a8b27a7dd2
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-07-25 23:34:42 +0600

    Started Services Section

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,9 +1,36 @@
-import React from 'react'
-import './services.css'
+import React from 'react';
+import './services.css';
+
+
+// const ServicesData = [
+//   {
+//     id: 1,
+//     image: Image1,
+//     title: "UI/UX design",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.",
+//   },
+//   {
+//     id: 2,
+//     image: Image2,
+//     title: "Web Development",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.",
+//   },
+//   {
+//     id: 3,
+//     image: Image3,
+//     title: "Photography",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetuer adipiscing elit aenean commodo ligula eget.",
+//   },
+// ]
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="services container section" id="services">
+      <h2 className="section_title">Services</h2>
+    </div>
   )
 }
 
