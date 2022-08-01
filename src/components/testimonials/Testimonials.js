13e411c77ebf7a4953994246bcd83348dfad6b29
Developer-Faras <forasuddin.freelancer@gmail.com>
2022-08-01 21:49:33 +0600

    Complete Testimonial Design

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './testimonials.css'
 
+import TestimonialCard from './TestimonialCard'
+import TestimonialData from './TestimonialData'
+
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <div className="testimonoals section container" id="testimonials">
+      <h2 className="section_title">{"Clients & Reviews"}</h2>
+
+      <div className="testimonial_data grid">
+        {TestimonialData.map((testimonial, index) => <TestimonialCard key={index} {...testimonial} />)}
+      </div>
+    </div>
   )
 }
 
